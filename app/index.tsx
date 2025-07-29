b5a0ebbdd9e78e44bfcbc4862f1846ff0b1614a0
KaderACAR <acarkader67@gmail.com>
2025-07-29 03:47:37 +0300

    nativewind

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Page = () => {
+  return (
+    <View className="" style={{backgroundColor: "black", flex: 1}}>
+        
+        <View style={{flex: 3, backgroundColor: "pink"}}></View>
+        <View style={{flex: 1, backgroundColor: "beige"}}></View>
+
+    </View>
+  )
+}
+
+export default Page
